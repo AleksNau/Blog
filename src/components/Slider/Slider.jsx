33fcf502ec50b2f6c300f121809f454c7b088038
AleksNau <workciti@yandex.ru>
2024-03-13 21:51:28 +0300

    изображения

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -3,12 +3,14 @@ import s from "./Slider.module.scss";
 import Container from "../../veaws/Container/Container.jsx";
 import Likes from "../Likes/Likes.jsx";
 import shorts from "../../publication-list/list.js";
+import im from "../../images/Promo.jpg"
 
 const Slider = () => {
+
     return (
         <section >
             <Container className={s.container}>
-                <div className={`${s.short} ${s.lead}`}>
+                <div className={`${s.short} ${s.lead}`} style={{ backgroundImage:`url(${im})` }}>
                     <h2 className={s.title}>{shorts[0].title}</h2>
                     <Likes name={s.about} text={s.text}/>
                 </div>
