c678c79e566ee4e7d0a597f6af89ef28bfd03eb9
AleksNau <workciti@yandex.ru>
2024-03-14 13:59:03 +0300

    news +favicon

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,11 +1,11 @@
 import s from "./News.module.scss";
-import image from "../../images/test.png"
 
-const News = () => (
+
+const News = ({link,title,text}) => (
     <div className={s.news}>
-        <img className={s.image} src={image} alt="Тестовое изображение"/>
-        <h2 className={s.title}>Новостной заголовок</h2>
-        <p className={s.paragraph}>FROG is a deflationary token with</p>
+        <img className={s.image} src={link} alt="Тестовое изображение"/>
+        <h2 className={s.title}>{title}</h2>
+        <p className={s.paragraph}>{text}</p>
     </div>
 )
 
--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import s from "./Popular.module.scss";
 import News from "../News/News.jsx";
+import shorts from "../../publication-list/list.js";
+import image from "../../images/test.png"
 
 const Popular = () => {
     return (
         <section className={s.container}>
             <h2 className={s.title}>Заголовок</h2>
-            <News/>
-            <News/>
-            <News/>
+            <News title={shorts[0].title} link={image} text={shorts[0].subtext}/>
+            <News title={shorts[1].title} link={shorts[1].link} text={shorts[1].subtext}/>
+            <News title={shorts[2].title} link={shorts[2].link} text={shorts[2].subtext}/>
         </section>
     );
 };
